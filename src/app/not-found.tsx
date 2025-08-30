8dc6416984e98c9819f89fb6aad58d10d72e978a
Shubham Kumar <shubhamkumar200328@gmail.com>
2025-08-30 16:10:20 +0530

    some basic correction

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,10 +7,11 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center px-4">
       <h1 className="text-6xl font-bold text-orange-600 mb-4">404</h1>
       <p className="text-xl text-gray-700 mb-8">Oops! Page not found.</p>
-      <Link href="/">
-        <a className="px-6 py-3 bg-orange-500 text-white rounded-full hover:bg-orange-600 transition">
-          Go back to Home
-        </a>
+      <Link
+        href="/"
+        className="px-6 py-3 bg-orange-500 text-white rounded-full hover:bg-orange-600 transition"
+      >
+        Go back to Home
       </Link>
     </div>
   );
--- src/app/user-profile/[[...user-profile]]/page.tsx
+++ src/app/user-profile/[[...user-profile]]/page.tsx
@@ -1,9 +1,24 @@
 import { UserProfile } from '@clerk/nextjs';
+import Image from 'next/image';
+import image from '@/assets/user.png';
 
 export default function UserProfilePage() {
   return (
-    <div className="flex justify-center items-center py-8">
-      <UserProfile path="/user-profile" />
+    <div className="flex flex-col items-center py-8 w-[100%] bg-white rounded-lg shadow-md">
+      <div className="flex flex-col items-center my-6">
+        <Image
+          className="w-20 h-20 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+          src={image}
+          alt="User avatar"
+          width={80}
+          height={80}
+        />
+        <h1 className="text-3xl font-bold mt-4 text-black">User Profile</h1>
+      </div>
+
+      <div className="w-full max-w-4xl">
+        <UserProfile path="/user-profile" />
+      </div>
     </div>
   );
 }
